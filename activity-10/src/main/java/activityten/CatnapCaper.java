39f7f67ab497ca3f30b21d4f16e14b835e705b89
Douglas Luman <dluman@douglasjluman.com>
2020-03-08 10:23:21 -0400

    Running different scenarios

--- activity-10/src/main/java/activityten/CatnapCaper.java
+++ activity-10/src/main/java/activityten/CatnapCaper.java
@@ -18,17 +18,33 @@ public class CatnapCaper {
     System.out.print("Does Alice defect [true/false]: ");
     boolean alice = input.nextBoolean();
     
+    System.out.println();
+    
+    if (alice) {
+      System.out.println("Alice defects!");
+    } else {
+      System.out.println("Alice stays quiet.");
+    }
+    
     System.out.print("Does Bob defect [true/false]: ");
     boolean bob = input.nextBoolean();
+    
+    System.out.println();
+    
+    if (bob) {
+      System.out.println("Bob defects!");
+    } else {
+      System.out.println("Bob stays quiet.");
+    }
     
     int aliceSentence = 0;
     int bobSentence = 0;
     
-    if(alice && !bob) {
+    if (alice && !bob) {
       bobSentence += 3;
-    } else if(!alice && bob) {
+    } else if (!alice && bob) {
       aliceSentence += 3;
-    } else if(!alice && !bob) {
+    } else if (!alice && !bob) {
       aliceSentence += 2;
       bobSentence += 2;
     } else {
@@ -36,7 +52,7 @@ public class CatnapCaper {
       aliceSentence += 1;
     }
     
-    if(aliceSentence == 0 || bobSentence == 0){
+    if (aliceSentence == 0 || bobSentence == 0) {
       System.out.println("And they catnap again!");
     }
   }
